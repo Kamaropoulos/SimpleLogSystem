a121adbb7e84d284c37f7c55fbec5348b883c604
KostasLifeboy <klifeboy@live.com>
2016-02-04 23:07:45 +0200

    Merged the whole source code in one header file

--- Log.h
+++ Log.h
@@ -4,20 +4,29 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
-#include <string>
-
-using namespace std;
 
 class Log {
 private:
-	ofstream LogFile;
+	std::ofstream LogFile;
 	char buff[20];
 	struct tm *sTm;
 
 public:
-	bool Initialize(char LogFileName[]);
-	void LogWrite(char level[], char message[]);
-	~Log();
+	bool Initialize(char LogFileName[]) {
+		LogFile.open(LogFileName, std::ios_base::app);
+		LogWrite("INFO", "The Event Log service was started.");
+		return true;
+	}
+	void LogWrite(char level[], char message[]) {
+		time_t now = time(0);
+		sTm = gmtime(&now);
+		strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", sTm);
+		LogFile << buff << " " << level << ": " << message << std::endl;
+	}
+	~Log() {
+		LogWrite("INFO", "The Event log service was stopped.");
+		LogFile.close();
+	}
 };
 
 #endif

b66cbd53170bcd9a8ac27a2055eca41b98361504
KostasLifeboy <klifeboy@live.com>
2016-02-04 22:36:26 +0200

    Fixed problem with linking

--- Log.h
+++ Log.h
@@ -1,8 +1,22 @@
-#pragma once
+#ifndef Log_H
+#define Log_H
+
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
 
 class Log {
+private:
+	ofstream LogFile;
+	char buff[20];
+	struct tm *sTm;
+
 public:
 	bool Initialize(char LogFileName[]);
-	~Log();
 	void LogWrite(char level[], char message[]);
+	~Log();
 };
+
+#endif
